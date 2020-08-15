6ad54cf0dd8bc11c59381002a3e31fa6adc21849
krebzsun <69425265+krebzsun@users.noreply.github.com>
2020-08-16 00:26:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@ https://github.com/P3TERX/Actions-OpenWrt
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
